85ac81038cb0704b969a306a6b87744417d691ce
rcolson243 <raphael.kasese@gmail.com>
2021-04-24 15:25:53 +0200

    partie detaill

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render , get_object_or_404
 from django.urls import reverse
 from .models import Category, Product
 
-from cart.forms import CartAddProductForm
+# from cart.forms import CartAddProductForm
 # Create your views here.
 
 
